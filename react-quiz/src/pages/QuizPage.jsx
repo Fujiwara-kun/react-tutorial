0cce5dea193bc88e29125eff5db5dbedfdae0dd8
kanazawa <kaka_gegez@protonmail.com>
2025-09-02 00:41:19 +0900

    クイズの正誤判定

--- react-quiz/src/pages/QuizPage.jsx
+++ react-quiz/src/pages/QuizPage.jsx
@@ -1,25 +1,32 @@
 import Display from "../components/display/Display.jsx"
 import quizData from "../data/quiz.js"
 import Button from "../components/button/Button.jsx"
+import { useState } from "react"
 export default function () {
-    const quizIndex = 0;
+    const { quizIndex, setQuizIndex } = useState(0);
+    const { answerLogs, setAnswerLogs } = useState([]);
+
+    const handleClick = (clickedIndex) => {
+        if (clickedIndex === quizData[quiziIndex].answerIndex) {
+            setAnswerLogs((prev) => [...prev, true])//正誤判定
+        } else {
+            setAnswerLogs((prev) => [...prev, false])//正誤判定
+        }
+        setQuizIndex((prev) => prev + 1);//クイズを一つ進める
+    }
     return (
         <>
             <Display>
                 {`01.${quizData[quizIndex].question}`}
             </Display>
-            <Button>
-                {quizData[quizIndex].option[0]}
-            </Button>
-            <Button>
-                {quizData[quizIndex].option[1]}
-            </Button>
-            <Button>
-                {quizData[quizIndex].option[2]}
-            </Button>
-            <Button>
-                {quizData[quizIndex].option[3]}
-            </Button>
+            {
+                quizData[quizIndex].option.map((option, index) => {
+                    return <Button key={`option-${index}`} onClick={() => handleClick(index)}>{option}</Button>
+                })
+            }
+
+
+
         </>
     )
 }
